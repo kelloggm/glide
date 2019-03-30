68d715b5eb8b22bfd7a307caad180455de22b82a
Martin Kellogg <kelloggm@cs.washington.edu>
2019-03-30 11:07:51 -0700

    setup script

---
+++ setup-cse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to setup a CSE machine to build TinyTravelTracker
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0/
+
+if [ ! -d android-sdk ]; then
+    mkdir android-sdk/
+    cd android-sdk/
+    wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+    unzip sdk-tools-linux-4333796.zip
+    cd ..
+fi
+
+export ANDROID_HOME=`pwd`/android-sdk/
+
+yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses
+
+./gradlew $1
+
