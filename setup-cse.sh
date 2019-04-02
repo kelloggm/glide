a534be35eae5fb371377bc6a4018f45674a5c28f
Martin Kellogg <kelloggm@cs.washington.edu>
2019-04-02 12:34:27 -0700

    full build

--- setup-cse.sh
+++ setup-cse.sh
@@ -16,5 +16,5 @@ export ANDROID_HOME=`pwd`/android-sdk/
 
 yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses
 
-./gradlew compileDebugJavaWithJavac
+./gradlew build --continue -Dorg.gradle.daemon=false
 
